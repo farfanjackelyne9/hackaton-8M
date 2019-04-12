b2f80a861be13e3281c08f8555ace3b61000b604
farfanjackelyne9 <farfanjackelyne9@gmail.com>
2019-04-12 17:53:11 -0500

    subiendo funcionalidad de los botones

--- main.js
+++ main.js
@@ -1,30 +1,23 @@
-const pagina1=document.getElementById("pagina1");
 const preguntas=document.getElementById("preguntas");
-const caja=document.getElementById("boxs");
-const resultado=document.getElementById("resultado");
-const orientacion=document.getElementById("orientacion");
+const boxs=document.getElementById("boxs");
+const pagina1=document.getElementById("pagina1");
 const testimonios=document.getElementById("testimonios");
-const directorio=document.getElementById("directorio");
 const cem=document.getElementById("cem");
-const resultado=document.getElementById("resultado");
 document.getElementById("btntest1").addEventListener("click",()=>{
-    pagina1.style.display="none";
-    caja.style.display="none";
-    resultado.style.display="none";
-    preguntas.style.display="block";
-});
+   pagina1.style.display="none";
+   boxs.style.display="none";
+   preguntas.style.display="block"; 
+})
 document.getElementById("btntesti").addEventListener("click",()=>{
     pagina1.style.display="none";
-    orientacion.style.display="none";
-    directorio.style.display="none";
+    boxs.style.display="none";
     testimonios.style.display="block";
 })
 document.getElementById("btnori").addEventListener("click",()=>{
     pagina1.style.display="none";
-    orientacion.style.display="none";
-    directorio.style.display="none";
-    testimonios.style.display="none";
+    boxs.style.display="none";
     cem.style.display="block";
 })
+
 
 
